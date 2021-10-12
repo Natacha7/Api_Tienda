a83771c973727bba26e204b63e816b4a8606971d
Natacha7 <jennynatacha@misena.edu.co>
2021-10-12 16:19:48 -0500

    Se agrega CRUD a clientes

--- routes/cliente.js
+++ routes/cliente.js
@@ -1,37 +1,26 @@
-import express from "express";
+import express from 'express';
 const router = express.Router();
 
-//importar modelo
-import Cliente from "../models/cliente";
+// Importar modelo
+import Cliente from '../models/cliente';
 
-//Ruta para consultar todos los productos
-router.get('/clientes', async(req,res)=> {
-    try {
-        const clienteDB = await Cliente.find();
-        res.json(clienteDB);
-    } catch (error) {
-        return res.status(400).json({
-            mensaje: 'Se presentó un error al consultar los clientes',
-            error
-        })
-    }
-});
-
-//Ruta para crear cliente
-router.post('/crear-cliente', async(req,res)=>{
+// Ruta para crear cliente
+router.post('/nuevo-cliente', async(req, res) => {
     const body = req.body.data;
+    // console.log(body);
     try {
-        const clienteDB = await  Cliente.create(body);
+        const clienteDB = await Cliente.create(body);
         res.status(200).json(clienteDB);
     } catch (error) {
         return res.status(400).json({
-            mensaje: 'Ocurrio un error al crear el cliente',
+            mensaje: 'Sucedió un error al crear el cliente',
             error
         })
     }
 });
+
 // Ruta para buscar cliente
-router.get('/buscar-cliente/:identificacion', async(req, res) => {
+router.get('/cliente/:identificacion', async(req, res) => {
     const identificacion = req.params.identificacion;
     try {
         console.log(identificacion)
